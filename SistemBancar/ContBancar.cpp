a2892800e91c89157603b69611516385e15f21d6
Toni <antonio.moldovan82@yahoo.com>
2021-12-03 19:18:54 +0200

    Created "Operatiuni" abstract class

--- SistemBancar/ContBancar.cpp
+++ SistemBancar/ContBancar.cpp
@@ -1,1 +1,26 @@
 #include "ContBancar.h"
+
+class Operatiuni
+{
+public:
+	virtual float getSumaTotala() = 0;
+	virtual float getDobanda() = 0;
+	virtual void depunere() = 0;
+	virtual void extragere() = 0;
+
+};
+
+class ContBancar
+{
+public:
+	ContBancar()
+	{
+	}
+
+	~ContBancar()
+	{
+	}
+
+private:
+
+};
